3f2fd2977cfef82feacf9599442fdf66e24a5bd1
Josh Hanson <joshhazn@gmail.com>
2015-11-26 01:01:26 -0500

    Its Lit

--- NBDWebApp/BidProcess.aspx
+++ NBDWebApp/BidProcess.aspx
@@ -3,9 +3,10 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
     <form id="form1" runat="server">
+
     <div class="bid-wrapper clearfix">
         <div class="form-group col-md-12">
-            <h1>Enter new client</h1>
+            <h1>Enter Client Info</h1>
         </div>
 
         <div class="form-group col-md-6">
@@ -52,5 +53,206 @@
         <asp:Button ID="btnInsert" CssClass="btn" runat="server" Text="Insert Client" />
         </div>
     </div>
+
+    <div class="bid-wrapper clearfix">
+        <div class="form-group col-md-12">
+            <h1>NBD Staff</h1>
+        </div>
+
+        <div class="form-group col-md-6">
+          <label id="lblBusiness" for="txtClient">Business</label>
+          <asp:TextBox ID="TextBox1" runat="server" class="form-control" placeholder="Business Name"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-6">
+          <label id="lblClient" for="txtClient">Primary Contact</label>
+          <asp:TextBox ID="TextBox2" runat="server" class="form-control" placeholder="Contact Name"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-6">
+            <label id="lblCity" for="txtCity">City</label>
+            <asp:TextBox ID="TextBox3" class="form-control" placeholder="City" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-4">
+            <label id="lblAddress" for="txtAddress">Address</label>
+            <asp:TextBox ID="TextBox4" class="form-control" placeholder="Address" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-2">
+            <label id="lblState" for="ddlState">State</label>
+            <asp:DropDownList  ID="DropDownList1" class="form-control" placeholder="State" runat="server">
+                <asp:ListItem Selected="True">Ontario</asp:ListItem>
+                <asp:ListItem>New York</asp:ListItem>
+                <asp:ListItem>Ohio</asp:ListItem>
+                <asp:ListItem>Texas</asp:ListItem>
+            </asp:DropDownList>
+        </div>      
+        
+        <div class="form-group col-md-6">
+          <label id="lblPhone" for="txtPhone">Phone</label>
+          <asp:TextBox ID="TextBox5" class="form-control" placeholder="Phone" runat="server"></asp:TextBox>
+        </div>
+        
+        <div class="form-group col-md-6">
+        <label id="lblEmail" for="txtEmail">Email</label>
+        <asp:TextBox ID="TextBox6" class="form-control" placeholder="Email" runat="server" TextMode="Email"></asp:TextBox>
+        </div>
+        
+        <div class="form-group col-md-12">
+        <asp:Button ID="Button1" CssClass="btn" runat="server" Text="Insert Client" />
+        </div>
+    </div>
+
+    <div class="bid-wrapper clearfix">
+        <div class="form-group col-md-12">
+            <h1>Project</h1>
+        </div>
+
+        <div class="form-group col-md-6">
+          <label id="lblBusiness" for="txtClient">Business</label>
+          <asp:TextBox ID="TextBox7" runat="server" class="form-control" placeholder="Business Name"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-6">
+          <label id="lblClient" for="txtClient">Primary Contact</label>
+          <asp:TextBox ID="TextBox8" runat="server" class="form-control" placeholder="Contact Name"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-6">
+            <label id="lblCity" for="txtCity">City</label>
+            <asp:TextBox ID="TextBox9" class="form-control" placeholder="City" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-4">
+            <label id="lblAddress" for="txtAddress">Address</label>
+            <asp:TextBox ID="TextBox10" class="form-control" placeholder="Address" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-2">
+            <label id="lblState" for="ddlState">State</label>
+            <asp:DropDownList  ID="DropDownList2" class="form-control" placeholder="State" runat="server">
+                <asp:ListItem Selected="True">Ontario</asp:ListItem>
+                <asp:ListItem>New York</asp:ListItem>
+                <asp:ListItem>Ohio</asp:ListItem>
+                <asp:ListItem>Texas</asp:ListItem>
+            </asp:DropDownList>
+        </div>      
+        
+        <div class="form-group col-md-6">
+          <label id="lblPhone" for="txtPhone">Phone</label>
+          <asp:TextBox ID="TextBox11" class="form-control" placeholder="Phone" runat="server"></asp:TextBox>
+        </div>
+        
+        <div class="form-group col-md-6">
+        <label id="lblEmail" for="txtEmail">Email</label>
+        <asp:TextBox ID="TextBox12" class="form-control" placeholder="Email" runat="server" TextMode="Email"></asp:TextBox>
+        </div>
+        
+        <div class="form-group col-md-12">
+        <asp:Button ID="Button2" CssClass="btn" runat="server" Text="Insert Client" />
+        </div>
+    </div>
+    
+    <div class="bid-wrapper clearfix">
+        <div class="form-group col-md-12">
+            <h1>Material Requirements</h1>
+        </div>
+
+        <div class="form-group col-md-6">
+          <label id="lblBusiness" for="txtClient">Business</label>
+          <asp:TextBox ID="TextBox13" runat="server" class="form-control" placeholder="Business Name"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-6">
+          <label id="lblClient" for="txtClient">Primary Contact</label>
+          <asp:TextBox ID="TextBox14" runat="server" class="form-control" placeholder="Contact Name"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-6">
+            <label id="lblCity" for="txtCity">City</label>
+            <asp:TextBox ID="TextBox15" class="form-control" placeholder="City" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-4">
+            <label id="lblAddress" for="txtAddress">Address</label>
+            <asp:TextBox ID="TextBox16" class="form-control" placeholder="Address" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-2">
+            <label id="lblState" for="ddlState">State</label>
+            <asp:DropDownList  ID="DropDownList3" class="form-control" placeholder="State" runat="server">
+                <asp:ListItem Selected="True">Ontario</asp:ListItem>
+                <asp:ListItem>New York</asp:ListItem>
+                <asp:ListItem>Ohio</asp:ListItem>
+                <asp:ListItem>Texas</asp:ListItem>
+            </asp:DropDownList>
+        </div>      
+        
+        <div class="form-group col-md-6">
+          <label id="lblPhone" for="txtPhone">Phone</label>
+          <asp:TextBox ID="TextBox17" class="form-control" placeholder="Phone" runat="server"></asp:TextBox>
+        </div>
+        
+        <div class="form-group col-md-6">
+        <label id="lblEmail" for="txtEmail">Email</label>
+        <asp:TextBox ID="TextBox18" class="form-control" placeholder="Email" runat="server" TextMode="Email"></asp:TextBox>
+        </div>
+        
+        <div class="form-group col-md-12">
+        <asp:Button ID="Button3" CssClass="btn" runat="server" Text="Insert Client" />
+        </div>
+    </div>
+
+    <div class="bid-wrapper clearfix">
+        <div class="form-group col-md-12">
+            <h1>Labour Requirements</h1>
+        </div>
+
+        <div class="form-group col-md-6">
+          <label id="lblBusiness" for="txtClient">Business</label>
+          <asp:TextBox ID="TextBox19" runat="server" class="form-control" placeholder="Business Name"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-6">
+          <label id="lblClient" for="txtClient">Primary Contact</label>
+          <asp:TextBox ID="TextBox20" runat="server" class="form-control" placeholder="Contact Name"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-6">
+            <label id="lblCity" for="txtCity">City</label>
+            <asp:TextBox ID="TextBox21" class="form-control" placeholder="City" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-4">
+            <label id="lblAddress" for="txtAddress">Address</label>
+            <asp:TextBox ID="TextBox22" class="form-control" placeholder="Address" runat="server"></asp:TextBox>
+        </div>
+
+        <div class="form-group col-md-2">
+            <label id="lblState" for="ddlState">State</label>
+            <asp:DropDownList  ID="DropDownList4" class="form-control" placeholder="State" runat="server">
+                <asp:ListItem Selected="True">Ontario</asp:ListItem>
+                <asp:ListItem>New York</asp:ListItem>
+                <asp:ListItem>Ohio</asp:ListItem>
+                <asp:ListItem>Texas</asp:ListItem>
+            </asp:DropDownList>
+        </div>      
+        
+        <div class="form-group col-md-6">
+          <label id="lblPhone" for="txtPhone">Phone</label>
+          <asp:TextBox ID="TextBox23" class="form-control" placeholder="Phone" runat="server"></asp:TextBox>
+        </div>
+        
+        <div class="form-group col-md-6">
+        <label id="lblEmail" for="txtEmail">Email</label>
+        <asp:TextBox ID="TextBox24" class="form-control" placeholder="Email" runat="server" TextMode="Email"></asp:TextBox>
+        </div>
+        
+        <div class="form-group col-md-12">
+        <asp:Button ID="Button4" CssClass="btn" runat="server" Text="Insert Client" />
+        </div>
+    </div>
+
     </form>
 </asp:Content>
--- NBDWebApp/BidProcess.aspx.designer.cs
+++ NBDWebApp/BidProcess.aspx.designer.cs
@@ -92,5 +92,293 @@ namespace NBDWebApp {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnInsert;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// TextBox3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        
+        /// <summary>
+        /// TextBox4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+        
+        /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// TextBox5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+        
+        /// <summary>
+        /// TextBox6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// TextBox7 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox7;
+        
+        /// <summary>
+        /// TextBox8 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox8;
+        
+        /// <summary>
+        /// TextBox9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox9;
+        
+        /// <summary>
+        /// TextBox10 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox10;
+        
+        /// <summary>
+        /// DropDownList2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
+        
+        /// <summary>
+        /// TextBox11 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox11;
+        
+        /// <summary>
+        /// TextBox12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox12;
+        
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+        
+        /// <summary>
+        /// TextBox13 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox13;
+        
+        /// <summary>
+        /// TextBox14 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox14;
+        
+        /// <summary>
+        /// TextBox15 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox15;
+        
+        /// <summary>
+        /// TextBox16 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox16;
+        
+        /// <summary>
+        /// DropDownList3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList3;
+        
+        /// <summary>
+        /// TextBox17 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox17;
+        
+        /// <summary>
+        /// TextBox18 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox18;
+        
+        /// <summary>
+        /// Button3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button3;
+        
+        /// <summary>
+        /// TextBox19 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox19;
+        
+        /// <summary>
+        /// TextBox20 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox20;
+        
+        /// <summary>
+        /// TextBox21 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox21;
+        
+        /// <summary>
+        /// TextBox22 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox22;
+        
+        /// <summary>
+        /// DropDownList4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList4;
+        
+        /// <summary>
+        /// TextBox23 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox23;
+        
+        /// <summary>
+        /// TextBox24 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox24;
+        
+        /// <summary>
+        /// Button4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button4;
     }
 }
