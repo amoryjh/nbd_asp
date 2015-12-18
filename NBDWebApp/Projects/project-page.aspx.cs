aff4fce05b1e3819542baf2c8f60274006238032
Josh Hanson <joshhazn@gmail.com>
2015-12-17 22:21:43 -0500

    Auto Populate project-page with ls fields

--- NBDWebApp/Projects/project-page.aspx.cs
+++ NBDWebApp/Projects/project-page.aspx.cs
@@ -11,7 +11,6 @@ namespace NBDWebApp.Projects
   public partial class london_mall : System.Web.UI.Page
   {
     NBD_DatabaseEntities db = new NBD_DatabaseEntities();
-    int projID = 1;
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -137,6 +136,85 @@ namespace NBDWebApp.Projects
             gvEstimatedHoursDBudget.DataBind();
         }
 
+        //////////////////////////////////////
+        //**********************************//
+        //**********************************//
+        //*******Fill With London Proj******//
+        //**********************************//
+        //**********************************//
+        //////////////////////////////////////
+        int id = 1;
+        var query = from client in db.CLIENTs where client.ID == id select client;
+
+        var query2 = from project in db.PROJECTs
+                     where project.ID == id
+                     select project;
+
+        txtClientBusinessDesign.Text = this.DropDownList2.SelectedItem.ToString();
+        this.txtDesignerPhoneDesign.Text = "4087753645";
+        this.txtSalesAssocPhoneDesign.Text = "4087753652";
+
+        foreach (var client in query)
+        {
+          //client info
+          this.txtClientAddressDesign.Text = client.cliAddress;
+          this.txtClientCityDesign.Text = client.CITY.city1;
+          this.txtClientPCodeDesign.Text = client.cliPCode;
+          this.txtClientPhoneDesign.Text = client.cliPhone;
+          this.txtClientFNameDesign.Text = client.cliConFName;
+          this.txtClientLNameDesign.Text = client.cliConLName;
+          this.txtClientPosDesign.Text = client.cliConPosition;
+
+
+        }
+
+        foreach (var q in query2)
+        {
+
+          //NBD staff
+          if (q.WORKER.wrkTypeID == 2)
+          {
+            //DESIGN BID
+            this.txtDesignerFNameDesign.Text = q.WORKER.wrkFName;
+            this.txtDesignerLNameDesign.Text = q.WORKER.wrkLName;
+
+          }
+          else if (q.WORKER.WORKER_TYPE.ID == 7)
+          {
+            //DESIGN BID
+            this.txtSalesAssocFNameDesign.Text = q.WORKER.wrkFName;
+            this.txtSalesAssocLNameDesign.Text = q.WORKER.wrkLName;
+
+
+
+          }
+          if (txtClientPhoneDesign.Text == "2262822677")
+          {
+            txtSalesAssocLNameDesign.Text = "Reinhardt";
+            txtSalesAssocFNameDesign.Text = "Bob";
+
+          }
+
+          //project
+          this.txtBidDateDesign.Text = Convert.ToString(q.projBidDate);
+          this.txtBidEstBeginDateDesign.Text = Convert.ToString(q.projEstStart);
+          this.txtBidEstComDateDesign.Text = Convert.ToString(q.projEstEnd);
+          this.txtProjSiteDesign.Text = q.projSite;
+          this.txtBidEstCostDesign.Text = q.projEstCost;
+          this.LlblTitle.Text = q.projName;
+
+          //Production PLAN Project
+          //project
+          //this.txtProjSiteProducton.Text = q.projName;
+          //this.txtProjSiteProduction.Text = q.projSite;
+          //this.txtBeginDateProduction.Text = q.projActStart;
+          //this.txtComDateProdcution.Text = q.projActEnd;
+          //this.txtBidCostProduction.Text = q.projEstCost;
+          //Total Expenses
+          //this.LblTotal.Text = q.projEstCost;
+
+
+        }
 
 
     }
